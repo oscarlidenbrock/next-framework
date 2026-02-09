81e9cb9b43a4a0f46c67c21919c94a7bf20a9603
Oscar Gonzalez <oscarlidenbrock@gmail.com>
2026-02-09 15:49:49 +0000

    add: bootstrap config parse workspaces

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,7 +1,25 @@
 <?php
 
+use Symfony\Component\Yaml\Yaml;
 
+$config = Yaml::parseFile('../config/app.yml');
+$workspace = null;
 
+/* Check workspaces */
+foreach ($config['workspaces'] as $workspaceName => $workspaceConfig) {
+    /* check hosts */
+    if (preg_match('/'.$workspaceConfig['host'].'/', $_SERVER['HTTP_HOST'])) {
+        $workspace = $workspaceName;
+        $config = $workspaceConfig;
+        break;
+    }
+}
+
+if (!$workspace) {
+    die("No workspace defined in config/app.yml");
+}
+
+pre($config, true);
 /**
  * Autoload Class Function
  */
