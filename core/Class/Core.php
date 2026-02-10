a902d97512cf0115e2e1bca87dd23705f6d368c8
Oscar Lidenbrock <oscarlidenbrock@gmail.com>
2026-02-10 16:44:31 +0000

    add: core parse modules config

--- core/Class/Core.php
+++ core/Class/Core.php
@@ -2,13 +2,31 @@
 
 namespace Core\Class;
 
-use PSpell\Config;
+use Symfony\Component\Yaml\Yaml;
 
 class Core
 {
     private $config;
+    private $modules = [];
     public function __construct($config)
     {
         $this->config = $config;
+
+        /* Parse modules */
+        if (isset($this->config['modules'])) {
+            foreach ($this->config['modules'] as $moduleKey => $moduleFolder) {
+                $modulePath = dirname(__FILE__).'/../../modules/'.$moduleFolder;
+
+                if (file_exists($modulePath.'/module.config.yml')) {
+                    $moduleConfig = Yaml::parseFile($modulePath.'/module.config.yml');
+                    $this->modules[$moduleKey] = [
+                        'config' => $moduleConfig
+                    ];
+                } else {
+                    /* TODO: Change error to standar errors */
+                    die('el modulo o su configuración no existen');
+                }
+            }
+        }
     }
 }
