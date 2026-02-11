00e9bc5a91c9fd20e1f669b5be7daa720491686f
Oscar Lidenbrock <oscarlidenbrock@gmail.com>
2026-02-12 00:21:51 +0100

    modify: parse routes and check http method to validate routes

--- core/Class/Router.php
+++ core/Class/Router.php
@@ -27,8 +27,31 @@ class Router
             }
         }
 
-        /* Get actual route */
+        /* Get actual route and method */
         $currentRoute = $_SERVER['REQUEST_URI'];
-        pre($currentRoute);
+        $currentMethod = $_SERVER['REQUEST_METHOD'];
+
+        /* Parse routes */
+        foreach ($this->routes as $regex => $route) {
+            $methodValid = false;
+
+            if (isset($route["route"]["method"])) {
+                if (is_array($route["route"]["method"])) {
+                    foreach ($route["route"]["method"] as $method) {
+                        if ($currentMethod == trim(strtoupper($method))) {
+                            $methodValid = true;
+                            break;
+                        }
+                    }
+                } else {
+                    if ($currentMethod == trim(strtoupper($route["route"]["method"]))) $methodValid = true;
+                }
+            } else {
+                /* If method is not defined, allow GET */
+                if ($currentMethod == "GET") $methodValid = true;
+            }
+
+            pre($methodValid);
+        }
     }
 }
