672795e0a863f02568a8f4b40b41d0ffe80e4caa
Oscar Lidenbrock <oscarlidenbrock@gmail.com>
2026-02-17 18:09:21 +0100

    add: get template file in controller->render() function

--- core/Class/Controller.php
+++ core/Class/Controller.php
@@ -5,6 +5,24 @@ namespace Core\Class;
 class Controller
 {
     public function render($view, $data = []) {
+        $reflection = new \ReflectionClass(static::class);
+
+        /* Get Child class path */
+        $filename = explode('/', $reflection->getFileName());
+        array_splice($filename, -3);
+        $filename = implode('/', $filename);
+
+        /* Get template data */
+        $filename .= '/templates/'.$view.'.twig';
+
+        if (file_exists($filename)) {
+            $data = file_get_contents($filename);
+        } else {
+            core()->error('Template not found');
+        }
+
+        pre($data);
+
         print "función render";
     }
 }
