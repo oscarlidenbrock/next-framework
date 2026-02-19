5ad7c4be59e50e6d33cfc3877fcbcff11528c50f
Oscar Lidenbrock <oscarlidenbrock@gmail.com>
2026-02-19 18:38:26 +0100

    refactor: render function to add render variable and process layout after page

--- modules/core/src/Class/Extend/Controller.php
+++ modules/core/src/Class/Extend/Controller.php
@@ -14,56 +14,39 @@ class Controller
         return $this;
     }
     public function render($view, $data = []) {
-        $render = ['html' => ''];
+        $html = '';
+        $data['render'] = [];
         $reflection = new \ReflectionClass(static::class);
 
         /* TODO: Change theme variable */
         $theme = "next";
-
-        /* Reserver variables */
-        $reserved = ['page'];
-
-        foreach ($reserved as $key) {
-            $data[$key] = '{!! '.$key.' !!}';
-        }
-
-        /* Get layout path */
-        if (file_exists(THEMES_PATH.'/'.$theme.'/layout/'.$this->layout.'.twig')) {
-            /* Render layout in custom theme */
-            $loader = new FilesystemLoader(THEMES_PATH.'/'.$theme.'/layout');
-            $twig = new Environment($loader);
-
-            $render['html'] = $twig->render($this->layout.'.twig', $data);
-        } elseif (file_exists(THEMES_PATH.'/default/layout/'.$this->layout.'.twig')) {
-            /* Render layout in default theme */
-            $loader = new FilesystemLoader(THEMES_PATH.'/default/layout');
-            $twig = new Environment($loader);
-
-            $render['html'] = $twig->render($this->layout.'.twig', $data);
-        }
 
         /* Get View path */
         $viewPath = explode('/', $reflection->getFileName());
         array_splice($viewPath, -3);
         $viewPath = implode('/', $viewPath).'/templates';
 
+        /* Initialize Twig for theme path */
+        $loader = new FilesystemLoader($viewPath);
+        $twig = new Environment($loader);
+
         /* Render view */
         if (file_exists($viewPath.'/'.$view.'.twig')) {
-            $loader = new FilesystemLoader($viewPath);
-            $twig = new Environment($loader);
-
-            $render['page'] = $twig->render($view.'.twig', $data);
+            $data['render']['page'] = $twig->render($view.'.twig', $data);
         } else {
             core()->error('Template not found');
         }
 
-        /* Get final html to render */
-        $html = $render['html'];
-        unset($render['html']);
-
-        foreach ($render as $key => $value) {
-            $html = str_replace('{!! '.$key.' !!}', $value, $html);
+        /* Set layout in custom or default theme */
+        if (file_exists(THEMES_PATH.'/'.$theme.'/layout/'.$this->layout.'.twig')) {
+            $loader = new FilesystemLoader(THEMES_PATH.'/'.$theme.'/layout');
+        } elseif (file_exists(THEMES_PATH.'/default/layout/'.$this->layout.'.twig')) {
+            $loader = new FilesystemLoader(THEMES_PATH.'/default/layout');
         }
+
+        /* Render layout */
+        $twig = new Environment($loader);
+        $html = $twig->render($this->layout.'.twig', $data);
 
         /* print $html */
         print($html);
