2e26285a88bec423b9ec7361e86e3710730feb8a
Oscar Lidenbrock <oscarlidenbrock@gmail.com>
2026-02-24 23:41:47 +0100

    add: core watchdog service

--- modules/core/src/Service/Watchdog.php
+++ modules/core/src/Service/Watchdog.php
@@ -4,5 +4,11 @@ namespace Core\Service;
 
 class Watchdog
 {
+    public function add($type, $error, $errorMessage, $die) {
+        $file = APP_PATH.'/var/log/watchdog.log';
+        $timestamp = date('Y-m-d H:i:s');
+        $text = '['.$timestamp."] ".$type.": ".$error.' ('.$errorMessage.')'.PHP_EOL;
 
+        file_put_contents($file, $text, FILE_APPEND);
+    }
 }
