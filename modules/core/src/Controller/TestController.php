c539d2756eca19cc611b140c2f00ada42a9a0107
Oscar Lidenbrock <oscarlidenbrock@gmail.com>
2026-02-26 16:17:53 +0100

    add: watchdog add function log for custom messages

--- modules/core/src/Controller/TestController.php
+++ modules/core/src/Controller/TestController.php
@@ -10,7 +10,6 @@ class TestController extends Controller
     }
 
     public function edit($var2, $var1) {
-        odds();
         $this->layout("html")->render('test/edit', ['var1' => $var1, 'var2' => $var2]);
     }
 }
